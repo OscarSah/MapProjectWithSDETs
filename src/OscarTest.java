b85db413370d0f89a4b996c818e403e87b888ccb
OscarSah <oscar@cybertekschool.com>
2024-02-08 19:45:56 -0500

    more commit

--- src/OscarTest.java
+++ src/OscarTest.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class OscarTest {
+public class OscarTest {
+    public static void main(String[] args) {
+        System.out.println("My first week at work, and so happy to contribute to my team");
+    }
 }
